859273d4933ff796cf0c3e57485e6ae8a231f3f4
Armin Sobhani <asobhani@sharcnet.ca>
2021-02-18 11:10:07 -0500

    Fix problem with building oneTBB and oneDPL

--- external/tbb.cmake
+++ external/tbb.cmake
@@ -4,11 +4,16 @@ execute_process(COMMAND git submodule update --init -- external/oneTBB
   WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
 )
 
-set(TBB_ROOT ${PROJECT_SOURCE_DIR}/external/oneTBB)
-list(APPEND CMAKE_MODULE_PATH ${PROJECT_SOURCE_DIR}/external/oneTBB/cmake)
-include(${PROJECT_SOURCE_DIR}/external/oneTBB/cmake/TBBBuild.cmake)
-tbb_build(TBB_ROOT ${TBB_ROOT} CONFIG_DIR TBB_DIR)
-find_package(TBB REQUIRED tbb)
+add_subdirectory(${CMAKE_CURRENT_SOURCE_DIR}/external/oneTBB
+  EXCLUDE_FROM_ALL
+)
+
+set(TBB_DIR ${CMAKE_CURRENT_BINARY_DIR}/external/oneTBB)
+# set(TBB_ROOT ${PROJECT_SOURCE_DIR}/external/oneTBB)
+# list(APPEND CMAKE_MODULE_PATH ${PROJECT_SOURCE_DIR}/external/oneTBB/cmake)
+# include(${PROJECT_SOURCE_DIR}/external/oneTBB/cmake/config_generation.cmake)
+# tbb_build(TBB_ROOT ${TBB_ROOT} CONFIG_DIR TBB_DIR)
+# find_package(TBB REQUIRED tbb)
 
 # removing .git in external/oneTBB to not bugged by git about modified files
 file(REMOVE_RECURSE "${PROJECT_SOURCE_DIR}/external/oneTBB/.git")
