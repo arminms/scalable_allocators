1ac775eed11955e0f3f8e4674b6fcd46e49474d2
Armin Sobhani <asobhani@sharcnet.ca>
2021-02-19 15:33:30 -0500

    Add scalable allocator for perf_generate_random

--- src/perf_generate_randoms.cpp
+++ src/perf_generate_randoms.cpp
@@ -9,10 +9,13 @@
 // #include <pstl/numeric>
 // #include <pstl/execution>
 
+
+#include <tbb/scalable_allocator.h>
+#include <tbb/cache_aligned_allocator.h>
+
 #include <benchmark/benchmark.h>
 
 #include <generate_randoms.hpp>
-#include <rmsd.hpp>
 
 template <typename Policy>
 static void GR_float(
@@ -21,10 +24,12 @@ static void GR_float(
 {
     size_t d = 3; // dimention
     std::mt19937 eng;
-    std::vector<float> A(st.range() * d);
 
     for (auto _ : st)
+    {
+        std::vector<float> A(st.range() * d);
         generate_randoms(ep, A.begin(), st.range(), d, eng);
+    }
 }
 
 BENCHMARK_CAPTURE(GR_float, seq, std::execution::seq)
@@ -33,6 +38,32 @@ BENCHMARK_CAPTURE(GR_float, seq, std::execution::seq)
 ->  Unit(benchmark::kMillisecond);
 
 BENCHMARK_CAPTURE(GR_float, par, std::execution::par)
+->  RangeMultiplier(2)
+->  Range(1<<20, 1<<24)
+->  Unit(benchmark::kMillisecond);
+
+template <typename Policy>
+static void SA_GR_float(
+    benchmark::State& st
+,   Policy ep)
+{
+    size_t d = 3; // dimention
+    std::mt19937 eng;
+
+
+    for (auto _ : st)
+    {
+        std::vector<float, tbb::scalable_allocator<float>> A(st.range() * d);
+        generate_randoms(ep, A.begin(), st.range(), d, eng);
+    }
+}
+
+BENCHMARK_CAPTURE(SA_GR_float, seq, std::execution::seq)
+->  RangeMultiplier(2)
+->  Range(1<<20, 1<<24)
+->  Unit(benchmark::kMillisecond);
+
+BENCHMARK_CAPTURE(SA_GR_float, par, std::execution::par)
 ->  RangeMultiplier(2)
 ->  Range(1<<20, 1<<24)
 ->  Unit(benchmark::kMillisecond);
