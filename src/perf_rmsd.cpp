b25624cdf2cd5e67b34cb373c98ce6e6986665df
Armin Sobhani <asobhani@sharcnet.ca>
2021-02-19 12:42:06 -0500

    Add src/perf_rmsd.cpp

---
+++ src/perf_rmsd.cpp
@@ -0,0 +1,102 @@
+#include <iostream>
+#include <random>
+
+#define TBB_PREVIEW_MEMORY_POOL 1
+#include <tbb/memory_pool.h>
+#include <tbb/scalable_allocator.h>
+#include <tbb/cache_aligned_allocator.h>
+
+#include <pstl/algorithm>
+#include <pstl/numeric>
+#include <pstl/execution>
+
+#include <benchmark/benchmark.h>
+
+#include <generate_randoms.hpp>
+#include <rmsd.hpp>
+
+template <typename Policy>
+static void RMSD_float(
+    benchmark::State& st
+,   Policy ep)
+{
+    size_t d = 3; // dimention
+    std::mt19937 eng;
+
+    for (auto _ : st)
+    {
+        std::vector<float> A(st.range() * d);
+        std::vector<float> B(st.range() * d);
+
+        generate_randoms(ep, A.begin(), st.range(), d, eng);
+        generate_randoms(ep, B.begin(), st.range(), d, eng);
+
+        float r = rmsd(ep, A.begin(), B.begin(), st.range(), d);
+        benchmark::DoNotOptimize(r);
+    }
+}
+
+// BENCHMARK_CAPTURE(RMSD_float, seq, std::execution::seq)
+// ->  RangeMultiplier(2)
+// ->  Range(1<<30, 1<<31)
+// ->  Unit(benchmark::kMillisecond);
+
+BENCHMARK_CAPTURE(RMSD_float, par, pstl::execution::par)
+->  RangeMultiplier(2)
+->  Range(1<<26, 1<<28)
+->  Unit(benchmark::kMillisecond);
+
+// BENCHMARK_CAPTURE(RMSD_float, unseq, pstl::execution::unseq)
+// ->  RangeMultiplier(2)
+// ->  Range(1<<20, 1<<24)
+// ->  Unit(benchmark::kMillisecond);
+
+// BENCHMARK_CAPTURE(RMSD_float, par_unseq, pstl::execution::par_unseq)
+// ->  RangeMultiplier(2)
+// ->  Range(1<<20, 1<<24)
+// ->  Unit(benchmark::kMillisecond);
+
+template <typename Policy>
+static void SA_RMSD_float(
+    benchmark::State& st
+,   Policy ep)
+{
+    size_t d = 3; // dimention
+    std::mt19937 eng;
+
+    for (auto _ : st)
+    {
+        std::vector<float, tbb::scalable_allocator<float>> A(st.range() * d);
+        std::vector<float, tbb::scalable_allocator<float>> B(st.range() * d);
+        // std::vector<float, tbb::cache_aligned_allocator<float>> A(st.range() * d);
+        // std::vector<float, tbb::cache_aligned_allocator<float>> B(st.range() * d);
+
+        generate_randoms(ep, A.begin(), st.range(), d, eng);
+        generate_randoms(ep, B.begin(), st.range(), d, eng);
+
+        float r = rmsd(ep, A.begin(), B.begin(), st.range(), d);
+        // benchmark::DoNotOptimize(r);
+    }
+}
+
+// BENCHMARK_CAPTURE(SA_RMSD_float, seq, std::execution::seq)
+// ->  RangeMultiplier(2)
+// ->  Range(1<<30, 1<<31)
+// ->  Unit(benchmark::kMillisecond);
+
+BENCHMARK_CAPTURE(SA_RMSD_float, par, pstl::execution::par)
+->  RangeMultiplier(2)
+->  Range(1<<26, 1<<28)
+->  Unit(benchmark::kMillisecond);
+
+// BENCHMARK_CAPTURE(SA_RMSD_float, unseq, pstl::execution::unseq)
+// ->  RangeMultiplier(2)
+// ->  Range(1<<20, 1<<24)
+// ->  Unit(benchmark::kMillisecond);
+
+// BENCHMARK_CAPTURE(SA_RMSD_float, par_unseq, pstl::execution::par_unseq)
+// ->  RangeMultiplier(2)
+// ->  Range(1<<20, 1<<24)
+// ->  Unit(benchmark::kMillisecond);
+
+BENCHMARK_MAIN();
